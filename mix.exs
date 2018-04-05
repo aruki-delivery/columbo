76d6d87585191f5d5c2503aa2d860d9f3179008e
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 18:15:06 +0100

    packaging

--- mix.exs
+++ mix.exs
@@ -5,10 +5,20 @@ defmodule Columbo.Mixfile do
     [app: :columbo,
       version: "0.1.0",
       language: :erlang,
-      deps: deps(Mix.env())]
+      deps: deps(Mix.env()),
+      description: "Erlang service discovery and tracking system for clusters",
+      package: package(),
+      source_url: "https://github.com/aruki-delivery/columbo",
+      homepage_url: "https://hex.pm/packages/columbo"]
   end
 
   defp deps(_) do
     []
+  end
+
+  def package do
+    [ maintainers: ["cblage"],
+      licenses: ["Apache License 2.0"],
+      links: %{"GitHub" => "https://github.com/aruki-delivery/columbo" } ]
   end
 end
---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "quaff",
+  description: "Erlang service discovery and tracking system for clusters",
+  version: "0.1.0",
+  keywords: ["erlang", "service", "discovery", "cluster", "clusters"],
+  licenses: [[name: "Apache License 2.0"]],
+  maintainers: [
+    [name: "Joaquim Rocha",  email: "jrocha.github@gmailbox.org"],
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "aruki-delivery/columbo", branch: "master"]])
